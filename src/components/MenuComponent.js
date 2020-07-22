417d7de84487fa88aded4aac9c52a5e942d91124
tanishqshek <tanishq.s@somaiya.edu>
2020-07-23 00:32:19 +0530

    Header and Footer

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component{
-    
-    render() {
+    function RenderMenuItem({ dish, onClick }){
+        return(
+            <Card key={dish.id} onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                <CardImgOverlay body className="ml-5">
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card> 
+        );
 
-        const menu = this.props.dishes.map((dish) => {
+    }
+
+    const Menu = (props) =>{
+        const menu = props.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1">
-                    <Card key={dish.id}
-                        onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-                        <CardImgOverlay body className="ml-5">
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
                 </div>
             );
         });
@@ -29,6 +32,6 @@ class Menu extends Component{
             </div>
          );
     }
-}
+        
 
 export default Menu;
